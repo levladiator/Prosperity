13217df1385fca4ce9482bb1aede856063aa3187
Vlad Dragutoiu <v.dragutoiu-1@student.tudelft.nl>
2024-04-08 13:25:15 +0200

    Added manual trading simulator for round 1

--- src/test.py
+++ src/test.py
@@ -1,6 +1,10 @@
+import random
+
 from datamodel import Listing, OrderDepth, Trade, TradingState
 from main import Trader
 import unittest
+import numpy as np
+from tqdm import tqdm
 
 
 class MyTestCase(unittest.TestCase):
@@ -70,10 +74,36 @@ class MyTestCase(unittest.TestCase):
 
     trader = Trader()
 
-    def test_something(self):
+    def test_main(self):
         self.trader.run(self.state)
         # assert True == True
 
+    def test_manual_trading(self):
+        probs = np.linspace(0, 1, 101)
+        probs = probs / np.sum(probs)
+        possible_prices = np.arange(900, 1001)
+        avg = []
+        for _ in tqdm(range(20)):
+            stats = []
+            for lowest_bid in range(900, 1001):
+                for highest_bid in range (lowest_bid, 1001):
+                    reserve_prices = random.choices(possible_prices, probs, k=10000)
+                    profit = 0
+                    for price in reserve_prices:
+                        if lowest_bid >= price:
+                            profit += 1000 - lowest_bid
+                        elif highest_bid >= price:
+                            profit += 1000 - highest_bid
+
+                    stats.append((profit, lowest_bid, highest_bid))
+
+            avg.append(sorted(stats, key=lambda x: x[0])[-1])
+
+        print(avg)
+        avg = np.mean(avg, axis=0)
+        print("Average best profit: ", avg[0])
+        print("Average best lower bound: ", avg[1])
+        print("Average best upper bound: ", avg[2])
 
 if __name__ == '__main__':
     unittest.main()
